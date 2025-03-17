26056f49c890419da36977a4f99ca72388449cfc
Adam Muyobo <muyoboadam@gmail.com>
2025-03-17 23:15:59 +0200

    Restructured the backend.

---
+++ Backend/nuka-pos/src/main/java/com/nuka/nuka_pos/NukaPosApplication.java
@@ -0,0 +1,13 @@
+package com.nuka.nuka_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NukaPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NukaPosApplication.class, args);
+	}
+
+}
