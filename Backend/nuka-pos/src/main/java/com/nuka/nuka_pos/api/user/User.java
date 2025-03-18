1a83a6101c5834ab6454cf84db70fbc90c772336
Adam Muyobo <muyoboadam@gmail.com>
2025-03-18 18:56:04 +0200

    almost died

--- Backend/nuka-pos/src/main/java/com/nuka/nuka_pos/api/user/User.java
+++ Backend/nuka-pos/src/main/java/com/nuka/nuka_pos/api/user/User.java
@@ -2,12 +2,6 @@ package com.nuka.nuka_pos.api.user;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -15,8 +9,9 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class User implements UserDetails {
+@EqualsAndHashCode
+@ToString
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -37,35 +32,6 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private Role role;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
-    }
 }
 
 
